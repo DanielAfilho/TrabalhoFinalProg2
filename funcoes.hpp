af680df4e79a2b0fd34de3e9d4ddb42222e4c352
edu5600 <eduardoaudascislowski@gmail.com>
2025-11-22 20:42:02 -0300

    Add files via upload

--- funcoes.hpp
+++ funcoes.hpp
@@ -1,37 +1,20 @@
 #include <iostream>
 #include <stdlib.h>
+#include <new>
 
 int qtd_alunos = 0;
-aluno* alunos;
 
 struct aluno
 {
     std::string nome;
     int serie;
-    char turma;
     int* notas;
-    aluno* alunos = nullptr; //guarda o próximo aluno
+    aluno* prox_aluno = nullptr; //guarda o próximo aluno
 };
 
-void cadastrar_aluno() 
-{
-    if (qtd_alunos == 0) {
-        qtd_alunos++;
-        alunos = (aluno*) malloc(sizeof(aluno));
-    }
-    else {
-        aluno* endereco_atual = alunos;
-        aluno* ponteiro_atual;
-        while (endereco_atual != nullptr) {
-            ponteiro_atual = endereco_atual;
-            endereco_atual = endereco_atual->alunos;
-        }
-        ponteiro_atual->alunos = (aluno*) malloc(sizeof(aluno));
-    }
-    return;
-}
+aluno* alunos = nullptr; //guarda o endereço do primeiro aluno
 
-void cadastro_do_aluno(aluno* aluno_editado) //onde o mano ai colocar os baguioo do estudante, caô.
+void cadastrar_aluno(aluno* aluno_editado)
 {
     std::cout << "Nome do aluno:  ";
     std::cin >> aluno_editado->nome;
@@ -39,22 +22,50 @@ void cadastro_do_aluno(aluno* aluno_editado) //onde o mano ai colocar os baguioo
     std::cout << "Série (ano):  ";
     std::cin >> aluno_editado->serie;
     std::cout << "\n";
-    std::cout << "Turma:  ";
-    std::cin >> aluno_editado->turma;
-    std::cout << "\n";
     return;
 }
+
+void criar_aluno() 
+{
+    if (qtd_alunos == 0) {
+        qtd_alunos++;
+        alunos = new aluno;
+        cadastrar_aluno(alunos);
+    }
+    else {
+        aluno* endereco_atual = alunos;
+        aluno* ponteiro_atual;
+        while (endereco_atual != nullptr) {
+            ponteiro_atual = endereco_atual;
+            endereco_atual = endereco_atual->prox_aluno;
+        }
+        ponteiro_atual->prox_aluno = new aluno;
+        cadastrar_aluno(ponteiro_atual->prox_aluno);
+    }
+    return;
+}
+
+void imprimir_alunos() {
+    aluno* aluno_atual = alunos;
+    while (aluno_atual != nullptr) {
+        std::cout << aluno_atual->nome << "    " << aluno_atual->serie;
+    }
+}
+
 void menu(){
     std::cout << "Bem vindo ao sistema de registro escolar\n\n";
+    std::cout << "1. Cadastrar aluno " << "\n";
+    std::cout << "2. Exibir alunos " << "\n";
+    std::cout << "3. Sair " << "\n";
     std::cout << "Escolha uma opção:\n";
     int opcao;
     std::cin >> opcao;
     switch (opcao){
         case 1:
-            cadastrar_aluno();
+            criar_aluno();
             break;
         case 2:
-            //buscar_aluno();
+            imprimir_alunos();
             break;
         case 3:
             exit(0);
